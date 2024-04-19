f4ea75a87d0b714d106990bbf60859064352c70c
Hem1144 <hemlaldulal7@gmail.com>
2024-04-19 08:20:33 +0545

    feat: Implemented logic of useRouter hook

--- app/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>This is Contact Us Page</div>;
-};
-
-export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+"use client";
 
+import { useRouter } from "next/navigation";
 const Home = () => {
+  const router = useRouter();
+
+  const navigate = (page: any) => {
+    router.push(page);
+  };
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <section>
+      <h1>useRouter</h1>
+      <button className=" border px-2 py-4" onClick={() => navigate("about")}>
+        Goto About Page
+      </button>
+      <br />
+      <button className=" border px-2 py-4" onClick={() => navigate("login")}>
+        Goto Login Page
+      </button>
+    </section>
   );
 };
 
